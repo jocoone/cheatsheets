5fe84df185251040fe32f43073e14bf44ef40d7c
Joey Comhaire <joey.comhaire@gmail.com>
2023-01-06 12:18:27 +0100

    p

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-const path = require("path");
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")]
+    includePaths: [path.join(__dirname, 'styles')]
   }
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,25 @@
-import "../styles/globals.scss";
-import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Raleway } from '@next/font/google';
+
+const raleway = Raleway({
+  style: ['normal', 'italic'],
+  subsets: ['latin']
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <div className="root">
+    <div className={raleway.className}>
       <header className="application-header">
         <nav>
           <Link className="home-link" href="/">
             <img src="/logo.svg" alt="logo" />
           </Link>
-          <Link
-            href="/howto"
-            className={router.pathname === "/howto" ? "active" : ""}
-          >
+          <Link href="/howto" className={router.pathname === '/howto' ? 'active' : ''}>
             create a cheatsheet
           </Link>
         </nav>
